32eadb842aa7399c10ed59831980c4b80e581f0d
Mathieu <mathieu762005@gmail.com>
2025-07-10 14:01:33 +0200

    maj

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        document.getElementById("container").innerHTML += `
-            <div class="card border-0" style="width: 15rem;">
-                <img src="https://image.tmdb.org/t/p/w500/${data.results[0].poster_path}" class="card-img-top rounded-0" id="c" alt="...">
+        for (i = 0; i < 20; i++) {
+            document.getElementById("container").innerHTML += `
+            <div class="card border-0 px-2" style="width: 15rem;">
+                <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}" class="card-img-top rounded-0" id="c" alt="">
                 <div class="card-body p-1">
-                    <h5 class="card-title text-white" id="titre">${data.results[0].title}</h5>
-                    <p class="date">date de sortie ${data.results[0].release_date}</p>
-                    <p class="text-white"><i class="bi bi-star-fill"></i> avis : ${Math.floor(data.results[0].vote_average)}</p>
+                    <h5 class="card-title text-white" id="titre">${data.results[i].title}</h5>
+                    <p class="date">date de sortie ${data.results[i].release_date}</p>
+                    <p class="text-white"><i class="bi bi-star-fill"></i> avis : ${Math.floor(data.results[i].vote_average)}</p>
                 </div>
             </div>
             `
-    }) 
+        }
+    })  
